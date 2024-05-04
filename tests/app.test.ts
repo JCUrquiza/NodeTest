fc98b0b491b9d8481082febd537518be69fff376
Juan <gurquiza8@gmail.com>
2024-05-04 14:00:34 -0500

    Test with template component, simple tests...

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -3,10 +3,19 @@
 // Act
 // Assert
 
-describe('App', () => {
+describe('Test in the App File', () => {
 
-    test('Should be true', () => {
-        expect(true).toBe(true);
+    test('Should be 30', () => {
+        // 1.- Arrange
+        const num1 = 20;
+        const num2 = 10;
+
+        // Act
+        const num3 = num1 + num2;
+
+        // Assert
+        expect(num3).toBe(30);
+        
     });
 
 });
